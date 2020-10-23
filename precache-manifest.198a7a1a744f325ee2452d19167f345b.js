086bcf9ca211e2a6041ed4a0cf7d25172caff3e1
nishantswarup <73164123+nishantswarup@users.noreply.github.com>
2020-10-23 16:50:38 +0000

    Deploying to gh-pages from  @ bc425d31ee8630de124488b34836b2e22e748286 🚀

---
+++ precache-manifest.198a7a1a744f325ee2452d19167f345b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d8b611df230ff2cd9501016124362e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b10c86e96bf9f9fb6f7",
+    "url": "/static/css/main.3b98bbeb.chunk.css"
+  },
+  {
+    "revision": "611a976a72ae9223539d",
+    "url": "/static/js/2.c09c94ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c09c94ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b10c86e96bf9f9fb6f7",
+    "url": "/static/js/main.70f07e82.chunk.js"
+  },
+  {
+    "revision": "c52aa85f0481664c545a",
+    "url": "/static/js/runtime-main.bd76a4c6.js"
+  },
+  {
+    "revision": "3242726a05656dbaa535f9c564cfa3e9",
+    "url": "/static/media/better-kubernetes.3242726a.svg"
+  },
+  {
+    "revision": "7a0f87caa1f0e402af66dc52f537f07d",
+    "url": "/static/media/body-background.7a0f87ca.png"
+  },
+  {
+    "revision": "7c7b1c76a41ef3508a4481931853cb44",
+    "url": "/static/media/built-in-prometheus-stats.7c7b1c76.svg"
+  },
+  {
+    "revision": "b0f068bbb61ed10e07a06e1b50a4cb23",
+    "url": "/static/media/context-aware-terminal.b0f068bb.svg"
+  },
+  {
+    "revision": "82f026e24b5490196a3c079f3d49d061",
+    "url": "/static/media/header-background.82f026e2.png"
+  },
+  {
+    "revision": "780695210c1bc7ce2562588ffbcb0953",
+    "url": "/static/media/header-lens.78069521.png"
+  },
+  {
+    "revision": "4d89dba8a292d974769ba2214b9d2128",
+    "url": "/static/media/logo.4d89dba8.svg"
+  },
+  {
+    "revision": "6cb8bb7999b641b4198498a4ed30d9b2",
+    "url": "/static/media/multi-cluster-management.6cb8bb79.svg"
+  },
+  {
+    "revision": "1e1405756d29efb30ee754ee22909bed",
+    "url": "/static/media/workspaces.1e140575.svg"
+  }
+]);
